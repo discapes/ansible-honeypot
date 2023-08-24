48303132d9a1fef0027dca1f256ca3b36d6b6928
Miika Tuominen <miika.km.tuominen@gmail.com>
2023-08-25 01:16:35 +0300

    allow logging sudo connections

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 # grep -v " - " is to exclude inactive hosts
 ip=$(sudo virsh net-dhcp-leases default | grep -v " - " | grep -Po '(\d{1,3}\.){3}\d{1,3}')
 sed "s/QQQ/$ip/" hosts.template > hosts
-ansible-playbook -i hosts site.yml 
+ansible-playbook -i hosts site.yml "$@"
