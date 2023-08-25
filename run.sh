a2e13528ed411d162b5c45071a79d5a4594bee01
Miika Tuominen <miika.km.tuominen@gmail.com>
2023-08-25 03:04:06 +0300

    use rathole instead of chisel, separate ansible tasks, dont hardcode ips

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # grep -v " - " is to exclude inactive hosts
 ip=$(sudo virsh net-dhcp-leases default | grep -v " - " | grep -Po '(\d{1,3}\.){3}\d{1,3}')
-sed "s/QQQ/$ip/" hosts.template > hosts
-ansible-playbook -i hosts site.yml "$@"
+sed "s/QQQ/$ip/" inv.tmpl.yml > inv.yml
+ansible-playbook -i inv.yml site.yml "$@"
